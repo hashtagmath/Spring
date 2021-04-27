9503febc71bc0a8abda803494ad0a6a98d6f638f
hashtagmath <matheussilvadossantos0@gmail.com>
2021-04-27 18:37:22 -0300

    Atividade2

---
+++ blogPessoal1/src/test/java/org/generation1/blogPessoal1/BlogPessoal1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation1.blogPessoal1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPessoal1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
