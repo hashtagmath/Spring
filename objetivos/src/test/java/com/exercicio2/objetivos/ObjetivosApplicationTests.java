aabba41698aa88bd9a27f7a4b396410a5f013816
hashtagmath <matheussilvadossantos0@gmail.com>
2021-04-27 13:09:20 -0300

    Atividade2

---
+++ objetivos/src/test/java/com/exercicio2/objetivos/ObjetivosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercicio2.objetivos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjetivosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
